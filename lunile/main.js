5e2419d8487854b17488b92cd53bf48dcbb88727
rares <jianu.rares30@gmail.com>
2018-01-28 19:56:34 +0200

    schimbare varzabruxelles

--- lunile/main.js
+++ lunile/main.js
@@ -145,7 +145,7 @@ window.addEventListener('click',function(e){
     // cerere XML ;
     var reqJson = new XMLHttpRequest();
 
-    reqJson.open('GET','http://localhost:3000/2');
+    reqJson.open('GET','vegetale.json');
 
     reqJson.onreadystatechange = function(){
 
--- produse/main.js
+++ produse/main.js
@@ -147,7 +147,7 @@ window.addEventListener('click',function(e){
     // cerere XML ;
     var reqJson = new XMLHttpRequest();
 
-    reqJson.open('GET','http://localhost:3000/1');
+    reqJson.open('GET','vegetale.json');
 
     reqJson.onreadystatechange = function(){
 
